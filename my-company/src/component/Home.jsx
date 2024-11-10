c8f8db319bed2ff73e129f539a6d5f94d5aed993
alxsoftware2024 <ashenafin2024@gmail.com>
2024-11-09 23:46:10 -0800

    Home page

--- my-company/src/component/Home.jsx
+++ my-company/src/component/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div><h1>Home</h1></div>
-  )
-}
+    return (
+      <div style={{ padding: '20px' }}>
+        <h1>Welcome to Our Company</h1>
+        <p>We are dedicated to delivering excellence in all our services.</p>
+      </div>
+    );
+  }
 
-export default Home
+  export default Home;
