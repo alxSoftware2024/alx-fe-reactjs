e2f885650a42f97bfca775e5314796df6537009b
alxsoftware2024 <ashenafin2024@gmail.com>
2024-11-09 21:36:13 -0800

    Calcultor in react

---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+function Counter() {
+    const[count,setCount]=useState(0);
+  return (
+    <div>
+<p>Current Count::{count}</p>
+    <button onClick={()=>setCount(count+1)}>Increment</button>  
+    <button>Decrement</button>  
+    <button>Reset</button>    
+    </div>
+  )
+}
+
+export default Counter
