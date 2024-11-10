780d22096e1baa3a00aeb2b0e7c0dd077bfc6389
alxsoftware2024 <ashenafin2024@gmail.com>
2024-11-09 21:41:00 -0800

    Decrement and Reset

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -6,8 +6,8 @@ function Counter() {
     <div>
 <p>Current Count::{count}</p>
     <button onClick={()=>setCount(count+1)}>Increment</button>  
-    <button>Decrement</button>  
-    <button>Reset</button>    
+    <button onClick={()=>setCount(count-1)}>Decrement</button>  
+    <button onClick={()=>setCount(0)}>Reset</button>    
     </div>
   )
 }
