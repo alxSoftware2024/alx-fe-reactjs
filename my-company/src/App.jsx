55f402c7e1c9d467ac7904664f6da5e86de95842
alxsoftware2024 <ashenafin2024@gmail.com>
2024-11-10 08:03:13 -0800

    Link the page

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -2,13 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from './componet/Home'
+import Contact from './componet/Contact'
+import About from './componet/About'
+import Services from './componet/Services'
+import {BrowserRouter as Router,Routes,Route,Link}from 'react-router-dom'
 function App() {
 
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <nav>
+<Link to='/'>Home</Link>
+<Link to='/About'>About Us</Link>
+<Link to='/Contact'>Contact Us</Link>
+<Link to='/Services'>Services</Link>
+
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Contact' element={<Contact/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Services' element={<Services/>}/>
+<Route path='*' element={<h1>Page Not Found</h1>}/>
+      </Routes>
+    </Router>
   )
 }
 
