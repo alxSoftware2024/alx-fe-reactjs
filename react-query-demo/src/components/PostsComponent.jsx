0139ae13b3b5764d8ac9a94859d01bb109d08f9d
alxsoftware2024 <ashenafin2024@gmail.com>
2024-11-23 11:32:41 -0800

    isError

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -7,9 +7,9 @@ const fetchData=async()=>{
 };
 function PostsComponent() {
     // Use the useQuery hook to handle data fetching and caching
-    const{data,error,isLoading}=useQuery('fetchData',fetchData);
+    const{data,isError,isLoading,fetchPosts}=useQuery('fetchData',fetchData);
     if(isLoading)return<div>Loading...</div>
-    if(error)return<div>Error Loading data..</div>
+    if(isError)return<div>Error Loading data..</div>
   return (
     <div>
             {data.map(item => (
