0796ea261ca0c1c6ee2b3908eba844d019e753c3
alxsoftware2024 <ashenafin2024@gmail.com>
2024-11-24 00:34:57 -0800

    Nested Routing

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import ProfileDetails from './ProfileDetails'
+import ProfileSettings from './ProfileSettings'
+import { BrowserRouter as  Router, Route, Routes, Link, useMatch } from 'react-router-dom';
 
 function Profile() {
   return (
