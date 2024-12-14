9683f7256cfe8aa098f5f85e522bd1f06edf53c5
alxsoftware2024 <ashenafin2024@gmail.com>
2024-12-13 23:28:26 -0800

    gitHubservice

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
 
-export const fetchUserData = async (username) => {
+export const fetchUserData = async (username, location, minRepos, page = 1) => {
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
+    // Construct the query parameters based on the user input
+    let query = `type:user`;
+    
+    if (username) query += `+in:login ${username}`;
+    if (location) query += `+location:${location}`;
+    if (minRepos) query += `+repos:>=${minRepos}`;
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${query}&page=${page}&per_page=10`
+    );
+
     return response.data;
   } catch (err) {
     throw new Error("Error fetching user data");
